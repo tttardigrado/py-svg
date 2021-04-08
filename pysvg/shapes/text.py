4e3cde03767b59aadaf40143f721926b39a5c286
Force4760 <gforce4760@gmail.com>
2021-04-08 14:52:35 +0100

    text path

--- pysvg/shapes/text.py
+++ pysvg/shapes/text.py
@@ -59,6 +59,67 @@ class TSpan(Text):
         super().__init__(canvas, text=text, x=x, y=y, dx=dx, dy=dy)
         self.tag = "tspan"
 
-class TextPath:
+class TextPath(InnerShape):
     """https://developer.mozilla.org/en-US/docs/Web/SVG/Element/textPath"""
-    pass
+    def __init__(self,canvas,text:str="", href:str=""):
+        super().__init__("textPath", canvas)
+        self.inner += text
+        self.adjust = "spacing"
+        self.length = 0
+        self.href = href
+        self.method = "align"
+        self.side = "left"
+        self.spacing = "exact"
+        self.offset = 0
+
+    def set_text(self,text:str):
+        self.inner = text
+
+    def set_adjust(self, keyword:str):
+        self.adjust = adjust
+    
+    def set_length(self,length:float):
+        self.length = length
+
+    def set_method(self,keyword:str):
+        self.mathod = keyword
+
+    def set_side(self,keyword:str):
+        self.side = keyword
+
+    def set_spacing(self,keyword:str):
+        self.spacing = keyword
+
+    def set_offset(self,length:float):
+        self.offset = length
+
+
+    def draw(self):
+        info = f"<{self.tag}"
+        info += f""" href="{self.href}" """
+
+        if self.adjust != "spacing":
+            info += f""" lengthAdjust='{self.adjust}' """
+
+        if self.method != "align":
+            info += f""" method="{self.method}" """
+        
+        if self.side != "left":
+            info += f""" side="{self.side}" """
+        
+        if self.spacing != "exact":
+            info += f""" spacing="{self.spacing}" """
+        
+        if self.offset:
+            info += f""" startOffset="{self.offset}" """
+        
+        if self.length:
+            info += f""" textLength="{self.length}" """
+        
+
+        info = self.id_attribute(info)
+        info = self.class_attribute(info)
+        info = self.style_attribute(info)
+        info = self.super_attribute(info)
+        info = self.inner_attribute(info)
+        return info
--- todo.py
+++ todo.py
@@ -1,4 +1,3 @@
-# TODO: textpath
 #TODO: defs g <symbol> <use> clipPath marker mask <style>
 #TODO: polygon path polyline
 #TODO: a foreignObject image
